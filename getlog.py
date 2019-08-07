69ac80bc29d90657b506dca7d2af5918f326b6e7
java2019 <java_2019@mail.ru>
2019-08-07 23:44:34 +0300

    *

---
+++ getlog.py
@@ -0,0 +1,31 @@
+import psycopg2
+
+
+def get_con() -> str:
+    conn: object = psycopg2.connect("dbname=log user=postgres host=localhost password=1")
+
+    # Open a cursor to perform database operations
+    cur = conn.cursor()
+
+    # Execute a command: this creates a new table
+    cur.execute("CREATE TABLE publictest (id serial PRIMARY KEY, num integer, data varchar);")
+
+    # Pass data to fill a query placeholders and let Psycopg perform
+    # the correct conversion (no more SQL injections!)
+    cur.execute("INSERT INTO log (id, phrase) VALUES (%s, %s)", (100, "abc'def"))
+
+    # Query the database and obtain data as Python objects
+    cur.execute("SELECT * FROM my_test;")
+    cur.fetchone()
+    (1, 100, "abc'def")
+
+    # Make the changes to the database persistent
+    conn.commit()
+
+    # Close communication with the database
+    cur.close()
+    conn.close()
+    return 'well done'
+
+
+print(get_con())
--- hello_flask.py
+++ hello_flask.py
@@ -1,11 +1,15 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, escape
 from vsearch import search4letters
+from getlog import get_con
+
 
 app = Flask(__name__)
 
+
 def log_request(req: 'flask_request', res: str) -> None:
     with open('vsearch.log', 'a') as log:
-        print(req, res, file=log)
+        print(req.form, req.remote_addr, req.user_agent ,res, file=log)
+
 
 @app.route('/search4', methods=['POST'])
 def do_search() -> 'html':
@@ -19,6 +23,25 @@ def do_search() -> 'html':
                            the_phrase=phrase,
                            the_letters=letters,
                            the_results=results,)
+
+
+@app.route('/viewlog')
+def view_the_log() -> 'html':
+    contents = []
+    with open('vsearch.log') as log:
+        for line in log:
+            contents.append([])
+            for item in line.split('|'):
+                contents[-1].append(escape(item))
+    titles = ('Form Data', 'Remote_addr', 'User_agent', 'Results')
+    return render_template('viewlog.html',
+                           the_title='View Log',
+                           the_row_titles=titles,
+                           the_data=contents,)
+
+
+def get_log() -> str:
+    get_con()
 
 
 @app.route('/')
