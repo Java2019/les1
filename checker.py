3437c83d1f6ff4e50706653be57670b88320c6fc
java2019 <java_2019@mail.ru>
2019-08-10 19:05:20 +0300

    *

--- checker.py
+++ checker.py
@@ -1,1 +1,9 @@
-def check_logge_in()
+from flask import session
+
+
+def check_logged_in(func):
+    def wrapper(*args, **kwargs):
+        if 'logged_in' in session:
+            return func(*args, **kwargs)
+        return 'You are NOT logged in.'
+    return wrapper
