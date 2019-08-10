ef7f24f4113fe2a01ba2891ac2e0ac34bfe42574
java2019 <java_2019@mail.ru>
2019-08-10 19:12:10 +0300

    *

---
+++ simple_webApp.py
@@ -0,0 +1,27 @@
+from flask import Flask, session
+
+app = Flask(__name__)
+app.secret_key = 'YouWillNeverGuessMySecretKey'
+
+
+@app.route('/login')
+def do_logon() -> str:
+    session['logged_in'] = True
+    return 'You are now logged in'
+
+
+@app.route('/logout')
+def do_logout()-> str:
+    session.pop('logged_in')
+    return 'You are now logged out'
+
+
+@app.route('/status')
+def check_status() -> str:
+    if 'logged_in' in session:
+        return 'You are currently logged in'
+    return 'You are NOT logged in'
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
