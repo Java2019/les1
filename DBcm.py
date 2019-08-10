43020a0df26efc3471ac8b8eccb1a8521b975b34
java2019 <java_2019@mail.ru>
2019-08-10 19:55:54 +0300

    *

--- DBcm.py
+++ DBcm.py
@@ -1,15 +1,23 @@
 import psycopg2
 
 
+class ConnectionError(Exception):
+    pass
+
+
 class UseDatabase:
 
     def __init__(self, config: dict) -> None:
         self.configuration = config
 
     def __enter__(self) -> 'cursor':
-        self.conn = psycopg2.connect(self.configuration)
-        self.cursor = self.conn.cursor()
-        return self.cursor
+        try:
+            self.conn = psycopg2.connect(self.configuration)
+            self.cursor = self.conn.cursor()
+            return self.cursor
+        except psycopg2.InterfaceError as err:
+            raise ConnectionError(err)
+
 
     def __exit__(self, exc_type, exc_val, exc_tb) -> None:
         self.conn.commit()
